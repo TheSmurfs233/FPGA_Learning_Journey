519a0cf946cbbd72651d5c92f13c3f180b4a5371
QAQ <17634737271@163.com>
2025-01-04 22:27:25 +0800

    修改文件

--- Pro/PLL/project/PLL_IPCoreFreqTransform.sim/sim_1/behav/xsim/xsim.dir/pll_top_tb_behav/webtalk/xsim_webtalk.tcl
+++ Pro/PLL/project/PLL_IPCoreFreqTransform.sim/sim_1/behav/xsim/xsim.dir/pll_top_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/FPGA_Learning_Journey/Pro/PLL/project/PLL_IPCoreFreqTransform.sim/sim_1/behav/xsim/xsim.dir/pll_top_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sat Jan  4 01:42:26 2025" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sat Jan  4 17:42:24 2025" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2019.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2708876" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "ea072d38-42e2-4972-90d2-968c94050173" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "1ceba3d956b54a409e1f65801c65e1db" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "4" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "6" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 5 7500F 6-Core Processor             " -context "user_environment"
@@ -22,12 +22,11 @@ webtalk_add_data -client project -key cpu_speed -value "3700 MHz" -context "user
 webtalk_add_data -client project -key total_processors -value "2" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "33.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
-webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "2 us" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "3 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "4" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "6.55_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "17104_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1856364677 -regid "" -xml D:/FPGA_Learning_Journey/Pro/PLL/project/PLL_IPCoreFreqTransform.sim/sim_1/behav/xsim/xsim.dir/pll_top_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/FPGA_Learning_Journey/Pro/PLL/project/PLL_IPCoreFreqTransform.sim/sim_1/behav/xsim/xsim.dir/pll_top_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/FPGA_Learning_Journey/Pro/PLL/project/PLL_IPCoreFreqTransform.sim/sim_1/behav/xsim/xsim.dir/pll_top_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "8980_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 3975761823 -regid "" -xml D:/FPGA_Learning_Journey/Pro/PLL/project/PLL_IPCoreFreqTransform.sim/sim_1/behav/xsim/xsim.dir/pll_top_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/FPGA_Learning_Journey/Pro/PLL/project/PLL_IPCoreFreqTransform.sim/sim_1/behav/xsim/xsim.dir/pll_top_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/FPGA_Learning_Journey/Pro/PLL/project/PLL_IPCoreFreqTransform.sim/sim_1/behav/xsim/xsim.dir/pll_top_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
--- Pro/PLL/src/pll_top.v
+++ Pro/PLL/src/pll_top.v
@@ -6,14 +6,14 @@ module pll_top (
     output clk_100m_180p,
     output clk_50m,
     output clk_25m
-);
-    clk_wiz_0 clk_wiz_0_ins(
-        .clk_in1(sys_clk),
-        .resetn(rst_n),
-        .locked(locked),
-        .clk_out1(clk_100m),
-        .clk_out2(clk_100m_180p),
-        .clk_out3(clk_50m),
-        .clk_out4(clk_25m)
-    );
-endmodule 
+  );
+  clk_wiz_0 clk_wiz_0_ins(
+              .clk_in1(sys_clk),
+              .resetn(rst_n),
+              .locked(locked),
+              .clk_out1(clk_100m),
+              .clk_out2(clk_100m_180p),
+              .clk_out3(clk_50m),
+              .clk_out4(clk_25m)
+            );
+endmodule
